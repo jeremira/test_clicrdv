c8cf662fc2c056a77501df4fe1d732331d0395f0
Jeremy Le Massu <jeremy.lemassu@clicrdv.com>
2018-01-12 18:23:55 +0100

    chore(openings) Add route, controller and model opening

---
+++ app/models/opening.rb
@@ -0,0 +1,4 @@
+class Opening < ActiveRecord::Base
+  # Association
+  belongs_to :calendars
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root 'calendars#index'
-  resources :calendars
-  resources :appointments
-  resources :interventions
+  root 'interventions#new'
+  resources :interventions, only: %i[new create deletion]
+
+  namespace :admin do
+    root 'calendars#index'
+    resources :calendars do
+      resources :openings
+    end
+    resources :appointments
+    resources :interventions
+  end
 end
